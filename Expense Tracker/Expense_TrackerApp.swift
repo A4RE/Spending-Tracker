ddc6ecf79767680ef03d7317f45e36e5a4ef1522
Andrei <aakoval01@gmail.com>
2024-05-29 18:28:02 +0300

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Андрей Коваленко on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
