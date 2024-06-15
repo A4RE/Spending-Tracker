26755a2fd6af6cb206a0d8cbee692619b3656160
Andrei <aakoval01@gmail.com>
2024-06-15 12:47:39 +0300

    Adds: 1. Create Custom TabBar 2. Some new components at HomeView

--- Expense Tracker/Views/HomeView/HomeView.swift
+++ Expense Tracker/Views/HomeView/HomeView.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            spacer
-            totalBalanceText
+        ZStack {
+            Color.cyan.ignoresSafeArea()
+            VStack {
+                titleStack
+                spendingText
+                    .padding(.top, 20.adjustSize())
+                spendingNumText
+                    .padding(.top, 10.adjustSize())
+                // TODO: Добавить график с тратами по дням
+                spacer
+            }
+            .padding(.horizontal, 16.adjustSize())
         }
     }
 }
@@ -22,10 +31,49 @@ extension HomeView {
         Spacer()
     }
     
-    var totalBalanceText: some View {
-        Text("Total Balance")
+    var titleStack: some View {
+        HStack {
+            Spacer()
+            dateLabel
+            Spacer()
+            createTopButton(icon: "gear") {
+                // TODO: Показ меню с выбором валюты
+                print("Settings")
+            }
+        }
+    }
+    
+    private func createTopButton(icon: String, action: @escaping () -> Void) -> some View {
+        Button(action: {
+            action()
+        }, label: {
+            Image(systemName: icon)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 22.adjustSize())
+        })
+        .foregroundStyle(.black)
+    }
+    
+    var dateLabel: some View {
+        VStack {
+            Text("Today is")
+                .font(.subheadline)
+            Text("\(Date.now.formatted(Date.FormatStyle().weekday())), \(Date.now, format: .dateTime.day(.defaultDigits).month())")
+                .font(.headline)
+        }
+    }
+    
+    var spendingText: some View {
+        Text("Total Spendings")
             .font(.system(size: 20.adjustSize(), weight: .semibold, design: .rounded))
             .foregroundColor(.secondary)
+    }
+    
+    var spendingNumText: some View {
+        Text("3000.50 ₽")
+            .font(.system(size: 50.adjustSize(), weight: .bold, design: .rounded))
+            .foregroundStyle(.primary)
     }
 }
 
--- Expense Tracker/Views/TabBar/TabBarView.swift
+++ Expense Tracker/Views/TabBar/TabBarView.swift
@@ -8,23 +8,73 @@
 import SwiftUI
 
 struct TabBarView: View {
+    
+    @State private var currentTab: Tab = .home
+    
     var body: some View {
-        TabView {
-            Text("1")
-                .tabItem {
-                    Label("1", systemImage: "xmark")
-                }
-            Text("2")
-                .tabItem {
-                    Label("2", systemImage: "xmark")
-                }
-            Text("3")
-                .tabItem {
-                    Label("3", systemImage: "xmark")
-                }
+        ZStack {
+            TabView(selection: $currentTab) {
+                HomeView()
+                    .tag(Tab.home)
+                Text("Transactions")
+                    .tag(Tab.transactions)
+                Text("Profile")
+                    .tag(Tab.profile)
+            }
+            .toolbar(.hidden, for: .tabBar)
+            
+            VStack {
+                Spacer()
+                tabBar
+            }
         }
     }
 }
+
+private extension TabBarView {
+    var tabBar: some View {
+        HStack {
+            Spacer()
+            tabBarButton(tab: .home, image: "house")
+            Spacer()
+            tabBarButton(tab: .transactions, image: "list.clipboard.fill")
+            Spacer()
+            tabBarButton(tab: .profile, image: "person.circle.fill")
+            Spacer()
+        }
+        .padding()
+        .frame(height: 70.adjustSize())
+        .background {
+            RoundedRectangle(cornerRadius: 36)
+                .fill(RadialGradient(gradient: Gradient(colors: [.gray, .white]), center: .center, startRadius: 2, endRadius: 300.adjustSize()))
+                .shadow(color: Color.black.opacity(0.5), radius: 8.adjustSize(), y: 2.adjustSize())
+        }
+        .padding(.horizontal, 50.adjustSize())
+    }
+    
+    func tabBarButton(tab: Tab, image: String) -> some View {
+        Button(action: {
+            withAnimation {
+                currentTab = tab
+            }
+        }, label: {
+            VStack(alignment: .center) {
+                Image(systemName: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: currentTab == tab ? 30.adjustSize() : 18.adjustSize())
+            }
+        })
+        .foregroundStyle(currentTab == tab ? .black : Color(uiColor: .darkGray))
+    }
+}
+
+enum Tab {
+    case home
+    case transactions
+    case profile
+}
+
 
 #Preview {
     TabBarView()
